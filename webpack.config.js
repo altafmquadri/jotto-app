4c60f5b752e9addff2172c82535d55aabc28e195
Altaf Quadri <altafmquadri@gmail.com>
2020-07-05 19:36:38 -0400

    boilerplate of the app setup

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+module.exports = (env) => {
+
+    const isProduction = env === 'production'
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js',
+            publicPath: '/dist/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'babel-loader',
+                        'eslint-loader'
+                    ]
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: '..index.html',
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            port: 3000,
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    }
+}
+
+
+
+
