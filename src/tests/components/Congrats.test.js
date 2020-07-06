8564693466854ebfa132c03cfe47834ea6bbc241
Altaf Quadri <altafmquadri@gmail.com>
2020-07-05 23:50:07 -0400

    added proptypes

--- src/tests/components/Congrats.test.js
+++ src/tests/components/Congrats.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme'
+import checkPropTypes from 'check-prop-types'
 import Congrats from '../../components/Congrats'
-import { findByTestAttr } from '../utils/testUtils'
+import { findByTestAttr, checkProps } from '../utils/testUtils'
 
+const defaultProps = { success: false }
 
 /**
  * Factory function to create a ShallowWrapper for the congrats component
@@ -10,11 +12,12 @@ import { findByTestAttr } from '../utils/testUtils'
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-    return shallow(<Congrats {...props} />)
+    const setupProps = { ...defaultProps, ...props }
+    return shallow(<Congrats {...setupProps} />)
 }
 
 test('renders without error', () => {
-    const wrapper = setup()
+    const wrapper = setup({ success: false })
     const component = findByTestAttr(wrapper, 'component-congrats')
     expect(component.length).toBe(1)
 })
@@ -29,4 +32,9 @@ test('renders non-empty congrats message when success prop is true', () => {
     const wrapper = setup({ success: true })
     const message = findByTestAttr(wrapper, 'congrats-message')
     expect(message.text().length).not.toBe(0)
+})
+
+test('does not throw warning with expected props', () => {
+    const expectedProps = { success: false }
+    checkProps(Congrats, expectedProps)
 })
--- src/tests/utils/testUtils.js
+++ src/tests/utils/testUtils.js
@@ -1,3 +1,6 @@
+import checkPropTypes from 'check-prop-types'
+import { Component } from 'react'
+
 /**
  * 
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
@@ -7,3 +10,13 @@
 export const findByTestAttr = (wrapper, val) => (
     wrapper.find(`[data-test="${val}"]`)
 )
+
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(
+        component.propTypes, 
+        conformingProps, 
+        'prop', 
+        component.name)
+        expect(propError).toBeUndefined()
+}
