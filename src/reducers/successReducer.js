99effcf97f16d36e4f61eaece7bb999c1dcd3b0b
Altaf Quadri <altafmquadri@gmail.com>
2020-07-07 17:41:21 -0400

    added tests for the reducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,11 +1,19 @@
+import { actionTypes } from '../actions/actionTypes'
 /**
  * @function successReducer
  * @param {array} state Array of guess words.
  * @param {object} action action to be reduced.
  * @returns {boolean} new success state.
  */
-const successReducer = (state, action) => {
-    return null
+const successReducer = (state = false, action) => {
+    switch (action.type) {
+        case (actionTypes.CORRECT_GUESS):
+            return true
+
+        default:
+            return false
+
+    }
 }
 
 export default successReducer;
--- src/tests/actions/actionTypes.test.js
+++ src/tests/actions/actionTypes.test.js
@@ -1,8 +1,8 @@
-import { correctGuess } from '../../actions/index'
+import { correctGuess } from '../../actions/actionTypes'
 
 describe('correctGuess', () => {
     test('return an action with type `CORRECT_GUESS`', () => {
         const action = correctGuess()
-        expect(action).toEqual({ type: 'CORRECT_GUESS'})
+        expect(action).toEqual({ type: 'CORRECT_GUESS' })
     })
 })
