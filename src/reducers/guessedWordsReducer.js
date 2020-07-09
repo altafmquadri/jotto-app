a4a1c23f1c59cc9fe092afa910ef6a75c9f9d6a5
Altaf Quadri <altafmquadri@gmail.com>
2020-07-08 22:40:00 -0400

    added guessWord action creator and wrote up reducer

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,12 +1,19 @@
-
+import {actionTypes} from '../actions/actionTypes'
 /**
  * @function guessedWordsReducer    
  * @param {array} state Array of guessed words
  * @param {object} action action to be reduced
  * @returns {array} new guessedWords state
  */
-const guessedWordsReducer = (state, action) => {
-    return null;
+const guessedWordsReducer = (state = [], action) => {
+    switch(action.type) {
+
+        case actionTypes.GUESS_WORD:
+            return [...state, action.payload]
+
+        default:
+            return state
+    }
 }
 
 export default guessedWordsReducer
