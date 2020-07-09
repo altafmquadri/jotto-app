7027d3a74d54d23f091ae311aef9a73949b77213
Altaf Quadri <altafmquadri@gmail.com>
2020-07-08 21:13:37 -0400

    added guessedWordReducer

---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,12 @@
+
+/**
+ * @function guessedWordsReducer    
+ * @param {array} state Array of guessed words
+ * @param {object} action action to be reduced
+ * @returns {array} new guessedWords state
+ */
+const guessedWordsReducer = (state, action) => {
+    return null;
+}
+
+export default guessedWordsReducer
---
+++ src/tests/integration/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../utils/testUtils'
+import { guessWord } from '../../actions/guessWord'
+
+describe('guessWord action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('updates state correctly for unsuccessful guess', () => {
+
+        })
+        test('updates state correctly for successful guess', () => {
+
+        })
+    })
+    describe('some guessed words', () => {
+        test('updates state correctly for unsuccessful guess', () => {
+
+        })
+        test('updates state correctly for successful guess', () => {
+
+        })
+    })
+})
